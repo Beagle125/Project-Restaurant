129025d044c98b1d435dd60065cbee08b4b17f9a
Cedric <cedric_daniel_chan@dlsu.edu.ph>
2026-05-02 12:26:56 +0800

    Changed homebg1 image

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import './styles.css';
-import * as myModules from './myModules.js';
+//import * as myModules from './myModules.js';
 
 const DOMManipulator = (() => {
     /*Determine the main container*/
     const mainContainer = document.getElementById('content');
-
-    console.log(myModules.homePageLoader);
 })();
