54c2f4ed424cffcf14270bf328aa5c5874eba211
Cedric <cedric_daniel_chan@dlsu.edu.ph>
2026-05-03 14:56:55 +0800

    Add functions for emptying the DOM and adding to the DOM for the homepage

--- src/main.js
+++ src/main.js
@@ -4,9 +4,30 @@ import * as myModules from './myModules.js';
 const DOMManipulator = (() => {
     /*Determine the main container*/
     const mainContainer = document.getElementById('content');
-    const moduleContainer = document.createElement('div');
+    const homeButton = document.getElementById('bHome');
+    const menuButton = document.getElementById('bMenu');
+    const reserveButton = document.getElementById('bReserve');
 
     /*Load the homepage*/
-    myModules.homePageLoader(moduleContainer);
-    mainContainer.appendChild(moduleContainer);
+    const addHomeModule = () => {
+        myModules.homePageLoader(mainContainer);
+    };
+
+
+    /*Remove the current content module*/
+    const removeModule = () => {
+        mainContainer.replaceChildren();
+    };
+
+
+    /*Initial load to the homepage*/
+    addHomeModule();
+    removeModule();
+
+    /*Event listeners*/
+    homeButton.addEventListener('click', () => {
+        removeModule();
+        addHomeModule();
+    });
+
 })();
